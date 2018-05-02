c87b84901348af1f6303475f9c1ac4e666b38adc
jaredad <35081849+jaredad@users.noreply.github.com>
2018-05-01 23:48:32 -0500

    Add files via upload

--- src/notes/Modes.java
+++ src/notes/Modes.java
@@ -11,13 +11,15 @@ public class Modes {
 	private String space = "                                                                                                        ";
 	private String mode;
 	private int num_of_boxes = 0;
+	private double nodeX = 0;
+	private double nodeY = 0;
 
 	public void labelChange(String labelName, Menu label) {
 		mode = space + labelName;
 		label.setText(mode);
 	}
 
-	public void createTextbox(Pane pane,double x, double y) {
+	public void createTextbox(Pane pane, double x, double y) {
 		TextArea textArea = new TextArea();
 		textArea.setPrefWidth(200);
 		textArea.setPrefHeight(200);
@@ -32,29 +34,34 @@ public class Modes {
 		modeReset(pane);
 		for (Node node : pane.getChildren()) {
 			if (node instanceof TextArea) {
-				dragAndDrop(pane, node);
+				dragAndDrop(pane, (TextArea) node);
 			}
 		}
 	}
 
+	public void dragAndDrop(Pane pane, TextArea node) {
+		modeReset(pane);
+		Node textAreaContent = node.lookup(".content");
 
-	public void dragAndDrop(Pane pane, Node node) {
-		node.setOnMouseClicked(event-> {
-			pane.setOnMouseMoved(event2-> {
-				pane.setOnMouseClicked(event3-> {
-					node.setLayoutX(event3.getSceneX());
-					node.setLayoutY(event3.getSceneY());	
-					pane.requestFocus();
-				});});});
+		textAreaContent.setOnMousePressed(event -> {
+			nodeX = textAreaContent.getLayoutX() - 100;
+			nodeY = textAreaContent.getLayoutY() - 100;
+		});
+
+		textAreaContent.setOnMouseDragged(event -> {
+			double dragX = event.getSceneX();
+			double dragY = event.getSceneY();
+			node.setLayoutX(nodeX + dragX);
+			node.setLayoutY(nodeY + dragY);
+		});
 	}
 
 	public void moveCanvasSetup(Pane pane, boolean isVertical) {
 		modeReset(pane);
-		pane.setOnScroll(event-> {
-			if(isVertical) {
+		pane.setOnScroll(event -> {
+			if (isVertical) {
 				scrollVertical(pane, event);
-			}
-			else {
+			} else {
 				scrollHorizontal(pane, event);
 			}
 		});
@@ -63,7 +70,7 @@ public class Modes {
 	public void scrollVertical(Pane pane, ScrollEvent event) {
 		for (Node node : pane.getChildren()) {
 			if (node instanceof TextArea) {
-				((TextArea)node).setTranslateY(node.getTranslateY() + event.getDeltaY());
+				((TextArea) node).setTranslateY(node.getTranslateY() + event.getDeltaY());
 			}
 		}
 	}
@@ -71,66 +78,86 @@ public class Modes {
 	public void scrollHorizontal(Pane pane, ScrollEvent event) {
 		for (Node node : pane.getChildren()) {
 			if (node instanceof TextArea) {
-				((TextArea)node).setTranslateX(node.getTranslateX() + event.getDeltaY());
+				((TextArea) node).setTranslateX(node.getTranslateX() + event.getDeltaY());
 			}
 		}
 	}
 
 	public void zoom(Pane pane) {
 		modeReset(pane);
-		pane.setOnScroll(event-> {
+		pane.setOnScroll(event -> {
 
-			double zoomFactor = 1.05;
 			double delta_y = event.getDeltaY();
 			double translateFactorX = 0;
 			double translateFactorY = 0;
-			double centerX = pane.getScene().getWindow().getWidth()/2;
-			double centerY = pane.getScene().getWindow().getHeight()/2;
-
-			if(delta_y>0) {
-				zoomFactor = 2.0-zoomFactor;
-			}
-
+			double centerX = pane.getScene().getWindow().getWidth() / 2;
+			System.out.println(centerX);
+			double centerY = pane.getScene().getWindow().getHeight() / 2;
+			System.out.println(centerY);
+			double zoomFactor = setupZoomFactor(delta_y);
 
 			for (Node node : pane.getChildren()) {
 				if (node instanceof TextArea) {
-					if ((node.getLayoutX() < centerX)&&(delta_y<0)) {
-						translateFactorX = -5.0;
-					} else if ((node.getLayoutX() > centerX)&&(delta_y<0)) {
-						translateFactorX = 5.0;
-					} else if ((node.getLayoutX() < centerX)&&(delta_y>0)) {
-						translateFactorX = 5.0;
-					} else if ((node.getLayoutX() > centerX)&&(delta_y>0)) {
-						translateFactorX = -5.0;
-					}
-
-					if ((node.getLayoutY() < centerY)&&(delta_y<0)) {
-						translateFactorY = -5.0;
-					} else if ((node.getLayoutY() > centerY)&&(delta_y<0)) {
-						translateFactorY = 5.0;
-					} else if ((node.getLayoutY() < centerY)&&(delta_y>0)) {
-						translateFactorY = 5.0;
-					} else if ((node.getLayoutY() > centerY)&&(delta_y>0)) {
-						translateFactorY = -5.0;
-					}
-
-					if(!((((TextArea)node).getWidth()<41 && delta_y>0)|| (((TextArea)node).getWidth()>300) && delta_y<0)){
-						((TextArea)node).setTranslateX(node.getTranslateX()+translateFactorX);
-						((TextArea)node).setTranslateY(node.getTranslateY()+translateFactorY);
-						((TextArea) node).setPrefWidth(((TextArea)node).getWidth()*zoomFactor);
-						((TextArea) node).setPrefHeight(((TextArea)node).getHeight()*zoomFactor);
+					double x = node.getLayoutX();
+					double y = node.getLayoutY();
+					translateFactorX = calculateTranslateX(x,centerX,delta_y);
+					translateFactorY = calculateTranslateY(y,centerY, delta_y);					
+					if (!((((TextArea) node).getWidth() < 41 && delta_y > 0)
+							|| (((TextArea) node).getWidth() > 300) && delta_y < 0)) {
+						((TextArea) node).setTranslateX(node.getTranslateX() + translateFactorX);
+						((TextArea) node).setTranslateY(node.getTranslateY() + translateFactorY);
+						((TextArea) node).setPrefWidth(((TextArea) node).getWidth() * zoomFactor);
+						((TextArea) node).setPrefHeight(((TextArea) node).getHeight() * zoomFactor);
 					}
 
 				}
 			}
 		});
-
 	}
+	
+	public double setupZoomFactor(double dy) {
+		double zoomFactor = 1.05;
+		if (dy > 0) {
+			zoomFactor = 2.0 - zoomFactor;
+		}
+		return zoomFactor;
+	}
+	
+	public double calculateTranslateX(double x, double centerX, double delta_y) {
+		double translateFactorX = 0.0;
+		if ((x < centerX) && (delta_y < 0)) {
+			translateFactorX = -5.0;
+		} else if ((x > centerX) && (delta_y < 0)) {
+			translateFactorX = 5.0;
+		} else if ((x < centerX) && (delta_y > 0)) {
+			translateFactorX = 5.0;
+		} else if ((x > centerX) && (delta_y > 0)) {
+			translateFactorX = -5.0;
+		}
+		return translateFactorX;
+	}
+	
+	public double calculateTranslateY(double y, double centerY, double delta_y) {
+		double translateFactorY = 0.0;
+		if ((y < centerY) && (delta_y < 0)) {
+			translateFactorY = -5.0;
+		} else if ((y > centerY) && (delta_y < 0)) {
+			translateFactorY = 5.0;
+		} else if ((y< centerY) && (delta_y > 0)) {
+			translateFactorY = 5.0;
+		} else if ((y > centerY) && (delta_y > 0)) {
+			translateFactorY = -5.0;
+		}
+		
+		return translateFactorY;
+	}
+	
 
 	public void modeReset(Pane pane) {
 		for (Node node : pane.getChildren()) {
 			if (node instanceof TextArea) {
-				((TextArea)node).setOnMouseClicked(null);
+				((TextArea) node).setOnMouseClicked(null);
+				((TextArea) node).setOnMouseDragged(null);
 				pane.setOnMouseMoved(null);
 				pane.setOnMouseClicked(null);
 				pane.setOnScroll(null);
@@ -138,17 +165,16 @@ public class Modes {
 			}
 		}
 	}
-	
+
 	public String getSpace() {
 		return space;
 	}
-	
+
 	public String getMode() {
 		return mode;
 	}
-	
+
 	public int numberOfBoxes() {
 		return num_of_boxes;
 	}
 }
-
--- src/notes/ModesTest.java
+++ src/notes/ModesTest.java
@@ -5,15 +5,61 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import javafx.scene.control.Menu;
 
-
 public class ModesTest {
 
-    @Test
-    public void testLabelChange() {
-    	Modes modes = new Modes();
+	@Test
+	public void testLabelChange() {
+		Modes modes = new Modes();
 		Menu label = new Menu();
 		modes.labelChange(modes.getSpace() + "Label", label);
-		assertEquals(modes.getSpace()+ modes.getSpace() + "Label", modes.getMode());
-    }
+		assertEquals(modes.getSpace() + modes.getSpace() + "Label", modes.getMode());
+	}
+
+	@Test
+	public void testZoomSetup() {
+		Modes modes = new Modes();
+		double dy1 = 1.0;
+		double dy2 = -1.0;
+		double zoomFactor = 0.0;
+		zoomFactor = modes.setupZoomFactor(dy1);
+		assertEquals(zoomFactor, 0.95);
+		zoomFactor = modes.setupZoomFactor(dy2);
+		assertEquals(zoomFactor, 1.05);
+	}
+
+	@Test
+	public void testcalculateTranslateX() {
+		Modes modes = new Modes();
+		double layoutX1 = 408.1;
+		double layoutX2 = 407.9;
+		double centerX = 408.0;
+		double dy1 = 1.0;
+		double dy2 = -1.0;
+		double translateX = modes.calculateTranslateX(layoutX1, centerX, dy1);
+		assertEquals(translateX, -5.0);
+		translateX = modes.calculateTranslateX(layoutX1, centerX, dy2);
+		assertEquals(translateX, 5.0);
+		translateX = modes.calculateTranslateX(layoutX2, centerX, dy1);
+		assertEquals(translateX, 5.0);
+		translateX = modes.calculateTranslateX(layoutX2, centerX, dy2);
+		assertEquals(translateX, -5.0);
+	}
+	
+	@Test
+	public void testcalculateTranslateY() {
+		Modes modes = new Modes();
+		double layoutY1 = 419.6;
+		double layoutY2 = 419.4;
+		double centerY = 419.5;
+		double dy1 = 1.0;
+		double dy2 = -1.0;
+		double translateX = modes.calculateTranslateX(layoutY1, centerY, dy1);
+		assertEquals(translateX, -5.0);
+		translateX = modes.calculateTranslateX(layoutY1, centerY, dy2);
+		assertEquals(translateX, 5.0);
+		translateX = modes.calculateTranslateX(layoutY2, centerY, dy1);
+		assertEquals(translateX, 5.0);
+		translateX = modes.calculateTranslateX(layoutY2, centerY, dy2);
+		assertEquals(translateX, -5.0);
+	}
 }
-    
